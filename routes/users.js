3f93dc448b6623695ed045c99a5d446d699736a0
Anuj Jain <lookforaj@gmail.com>
2017-12-20 15:57:46 +0530

    Added form validations
    Added validations in registration form

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var multer = require('multer');
+var upload = multer({ dest: './uploads' })
 var router = express.Router();
 
 /* GET users listing. */
@@ -14,4 +16,67 @@ router.get('/login', function(req, res, next) {
   res.render('login', {title: 'Login'});
 });
 
+router.post('/register', upload.single('profileimage'), function (req, res, next) {
+  // Get the form values
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  var password2 = req.body.password2;
+
+  // Check for image field
+  if(req.file) {
+    console.log('Uploading file...');
+
+    // File info
+    var profileImageOriginalName = req.file.originalname;
+    var profileImageName = req.file.name;
+    var profileImageMime = req.file.mimetype;
+    var profileImagePath = req.file.path;
+    var profileImageExt = req.file.extension;
+    var profileImageSize = req.file.size;
+  } else {
+    var profileImageName = 'noimage.png';
+  }
+
+  // Form Validation
+  req.checkBody('name', 'Name field is required').notEmpty();
+  req.checkBody('email', 'Email field is required').notEmpty();
+  req.checkBody('email', 'Email not valid').isEmail();
+  req.checkBody('username', 'Username field is required').notEmpty();
+  req.checkBody('password', 'Password field is required').notEmpty();
+  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+  // Check for errors
+  var errors = req.validationErrors();
+  if(errors) {
+    res.render('register', {
+      errors: errors,
+      name: name,
+      email: email,
+      username: username,
+      password: password,
+      password2: password2
+    });
+  } else {
+    var newUser = new User({
+      name: name,
+      email: email,
+      username: username,
+      password: password,
+      profileImage: profileImageName
+    });
+
+    // Create Use
+    /*User.createUser(newUser , function(err, user) {
+      if(err) throw err;
+      console.log(user);
+    });*/
+
+    // Success message
+    req.flash('success','You are now registered and may login!');
+    res.location('/');
+    res.redirect('/');
+  }
+});
 module.exports = router;
